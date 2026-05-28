18546520485ea1f35289f03db126b22ba4598bd4
looolidasuki <bili.23niang@gmail.com>
2026-05-29 05:03:37 +0800

    Delete Models directory

--- Models/Warehouse.cs
+++
@@ -1,9 +0,0 @@
-namespace Sales_user.Models
-{
-    public class Warehouse
-    {
-        public long WarehouseID { get; set; }
-        public string WarehouseName { get; set; }
-        public string WarehouseAddress { get; set; }
-    }
-}
